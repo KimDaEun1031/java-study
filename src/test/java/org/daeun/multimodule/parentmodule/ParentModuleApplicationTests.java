551ea6f4c8f725f93149aec58c253daf97b0e018
김다은 <73865700+KimDaEun1031@users.noreply.github.com>
2021-07-21 16:02:53 +0900

    Initial commit

---
+++ src/test/java/org/daeun/multimodule/parentmodule/ParentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daeun.multimodule.parentmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
